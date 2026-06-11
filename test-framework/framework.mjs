9ae324bdaa5798a11511cced7171bb5e9b45acc1
gmega <giuliano.mega@gmail.com>
2026-06-10 21:05:44 -0300

    drain streams to avoid backpressure from hanging the process

--- test-framework/framework.mjs
+++ test-framework/framework.mjs
@@ -223,6 +223,9 @@ function launchApp(appBin, verbose = false) {
   if (verbose) {
     child.stdout.on("data", (d) => process.stderr.write(`[app:out] ${d}`));
     child.stderr.on("data", (d) => process.stderr.write(`[app:err] ${d}`));
+  } else {
+    child.stdout.resume();
+    child.stderr.resume();
   }
 
   child.on("exit", (code) => {
